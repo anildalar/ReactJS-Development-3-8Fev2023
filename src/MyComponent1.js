6af503729088f084b22d09eea26945e708cfc573
unknown <dollorinfotech@gmail.com>
2023-02-08 09:47:58 +0530

    commit 1

---
+++ src/MyComponent1.js
@@ -0,0 +1,19 @@
+//1. Import Area
+
+// import something from somelocation/somelibrary
+
+import MyComponent2 from './MyComponent2';
+
+
+//2. Function defination area
+function MyComponent1(){ // PascalCase
+    //Every function return something
+    return <div>
+                <h1>WELCOME again4</h1>
+                <MyComponent2 />
+            </div>
+}
+
+//3. Export Area
+export default MyComponent1;
+
---
+++ src/MyComponent2.js
@@ -0,0 +1,14 @@
+//1. Import Area
+
+
+//2.Function Defination Area
+function MyComponent2(){
+    //Every function return something
+    return <span>Hello From MyComponent2</span>
+    // JSX (Javascript XML) Syntax
+}
+
+
+//3.Export
+
+export default MyComponent2;
